c6afbd76a183950d635322822b11af344e2dd6a4
Clark Williams <theclarkwilliams@gmail.com>
2019-07-23 18:53:33 -0600

    added another layer of sorting, completed tasks are at bottom of lists

--- todo/src/components/List.js
+++ todo/src/components/List.js
@@ -4,13 +4,16 @@ import Todo from './Todo';
 const List = props => {
     console.log(props.todos);
     let sortedProps = props.todos.sort((a, b) => {
-        return b.important - a.important
+        return b.important - a.important;
+    })
+    let compSortedProps = sortedProps.sort((a, b) => {
+        return a.completed - b.completed;
     })
     return (
         <div className="list">
             <h1>To-Do List</h1>
             {/* {props.todos.map(todo => { */}
-            {sortedProps.map(todo => {
+            {compSortedProps.map(todo => {
                 return <Todo 
                     todo={todo} 
                     key={todo.id} 
